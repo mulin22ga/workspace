dd9f9849450441cb563b3cf56893f7c445a7aadf
mulin22ga <happy.wang992@gmai.com>
2018-01-16 14:31:17 +0800

    Niner+Bubble

---
+++ exam/src/exam/Niner.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class Niner {
+
+	public static void main(String[] args) {
+		for (int x = 1; x < 6; x++) {
+			for (int y = 2; y < 6; y++) {
+				System.out.print(y + "X" + x + "=" + (x * y) + "\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
