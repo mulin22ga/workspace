68e3086b161044d42cc94ff180383afd67454921
mulin22ga <happy.wang992@gmai.com>
2018-01-16 15:32:56 +0800

    final

---
+++ exam/src/exam/Drink.java
@@ -0,0 +1,11 @@
+package exam;
+
+public class Drink {
+	String drink;
+	int price;
+
+	public Drink(String drink, int price) {
+		this.drink = drink;
+		this.price = price;
+	}
+}
---
+++ exam/src/exam/VendingMachine.java
@@ -0,0 +1,14 @@
+package exam;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		System.out.println("請輸入金額(限1,5,10元)");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+Drink d1 = new Drink("a",10 );
+	}
+
+}
